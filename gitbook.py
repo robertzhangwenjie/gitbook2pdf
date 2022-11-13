40e334cc0acb545ac701555988af362245d4cc36
robertzhangwenjie <1648855816@qq.com>
2022-11-13 12:36:28 +0800

    chore: support specify the file name generated

--- gitbook.py
+++ gitbook.py
@@ -2,4 +2,5 @@ import sys
 from gitbook2pdf import Gitbook2PDF
 if __name__ == '__main__':
     url = sys.argv[1]
-    Gitbook2PDF(url).run()
+    fname = sys.argv[2]
+    Gitbook2PDF(url,fname).run()
